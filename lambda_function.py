a9c67faef4ab3b1b7ee3acf855fb758a517e083d
jtschuwirth <jtschuwirth@uc.cl>
2023-08-02 19:55:43 -0400

    seller deployer working

--- lambda_function.py
+++ lambda_function.py
@@ -6,27 +6,29 @@ def handler(event, context):
     settings_table = init_settings_table()
     settings = settings_table.get_item(Key={"key_": "seller_settings"})["Item"]
     enabled = settings["enabled"]
+    current_invocation = int(settings["current_invocation"])
+    target_invocation = int(settings["target_invocations"])
     if not enabled: return "Seller is disabled"
-    current_invocation = settings["current_invocation"]+1
     settings_table.update_item(
         Key={"key_": "seller_settings"},
         UpdateExpression="SET current_invocation = :current_invocation",
         ExpressionAttributeValues={
-            ":current_invocation": current_invocation
+            ":current_invocation": current_invocation+1
         }
     )
-    if current_invocation < settings["target_invocations"]: return "Not enough invocations"
-    settings_table.update_item(
-        Key={"key_": "seller_settings"},
-        UpdateExpression="SET current_invocation = :current_invocation",
-        ExpressionAttributeValues={
-            ":current_invocation": 0
-        }
-    )
+    if target_invocation-1 <= current_invocation: 
+        settings_table.update_item(
+            Key={"key_": "seller_settings"},
+            UpdateExpression="SET current_invocation = :current_invocation",
+            ExpressionAttributeValues={
+                ":current_invocation": 0
+            }
+        )
     c=0
     accounts_to_manage = []
     account_groups = []
     for account in get_accounts():
+        if int(account, 16)%target_invocation != current_invocation: continue
         if c==10: 
             account_groups.append(accounts_to_manage)
             accounts_to_manage = []
